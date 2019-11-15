bd7103ea7db685a091ccbb1d8a3b37d051fa3c26
Alberto Delgado <alu0100889772@ull.edu.es>
2019-11-15 03:32:54 +0000

    Metodo to_s realizado

--- lib/Alimentos/Alimento.rb
+++ lib/Alimentos/Alimento.rb
@@ -7,4 +7,17 @@ class Alimento
 	def initialize(nombre, proteina, carb_hidr, lipido, gei, terreno)
 		@nombre, @proteina, @carb_hidr, @lipido, @gei, @terreno = nombre, proteina, carb_hidr, lipido, gei, terreno
 	end
+
+	def to_s
+		result = ""
+
+		result += nombre + ":\n"
+		result += "\tProteínas: " + proteina.to_s + "\n"
+		result += "\tCarbohidratos: " + carb_hidr.to_s + "\n"
+		result += "\tLípidos: " + lipido.to_s + "\n"
+		result += "\tGEI: " + gei.to_s + "\n"
+		result += "\tTerreno: " + terreno.to_s + "\n"
+
+		result
+	end
 end
